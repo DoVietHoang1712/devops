a1ba77fd37c853a3bab9316bece9442ea64f878d
DoVietHoang1712 <hoang171220001@gmail.com>
2022-05-09 22:41:51 +0700

    Add gitignore

---
+++ modules/cluster/variable.tf
@@ -0,0 +1,19 @@
+variable "serviceprinciple_id" {
+  
+}
+
+variable "serviceprinciple_key" {
+  
+}
+
+variable "location" {
+  default = "southeastasia"
+}
+
+variable "kubernetes_version" {
+  default = "1.21.5"
+}
+
+variable "ssh_key" {
+  
+}
---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "serviceprinciple_id" {
+  
+}
+
+variable "serviceprinciple_key" {
+  
+}
+
+variable "tenant_id" {
+  
+}
+
+variable "subscription_id" {
+  
+}
+
+variable "location" {
+  default = "southeastasia"
+}
+
+variable "kubernetes_version" {
+  default = "1.21.7"
+}
+
+variable "ssh_key" {
+  
+}
